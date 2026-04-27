f889eacaadb6b98ca1478a3b7230eed4c46e895e
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-04-27 15:54:30 -0500

    feat: render counters dynamically using map and fix state handlers

--- react-practice-lab/src/App.jsx
+++ react-practice-lab/src/App.jsx
@@ -1,54 +1,71 @@
 import { useState } from 'react';
 import Counter from './components/Counter.jsx';
+import './App.css';
 
 const App = () => {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(10);
 
-  const handleIncrement = () => {
+  const handleCount1Increment = () => {
     setCount1((prev) => prev + 1);
   };
 
-  const handleDecrement = () => {
+  const handleCount1Decrement = () => {
     setCount1((prev) => prev - 1);
   };
 
-  const handleReset = () => {
+  const handleCount1Reset = () => {
     setCount1(0);
   };
 
-  const handleIncrement1 = () => {
-    setCount2((prev) => prev - 5);
+  const handleCount2Increment = () => {
+    setCount2((prev) => prev + 1);
   };
 
-  const handleDecrement1 = () => {
-    setCount2((prev) => prev - 5);
+  const handleCount2Decrement = () => {
+    setCount2((prev) => prev - 1);
   };
 
-  const handleReset1 = () => {
-    setCount2(10);
+  const handleCount2Reset = () => {
+    setCount2(0);
   };
+
+  const counters = [
+    {
+      id: 1,
+      title: 'Passenger Counter',
+      count: count1,
+      onIncrement: handleCount1Increment,
+      onDecrement: handleCount1Decrement,
+      onReset: handleCount1Reset,
+    },
+    {
+      id: 2,
+      title: 'Student Counter',
+      count: count2,
+      onIncrement: handleCount2Increment,
+      onDecrement: handleCount2Decrement,
+      onReset: handleCount2Reset,
+    },
+  ];
 
   return (
     <main>
       <h1>React Practice Lab</h1>
 
       <div className="counter-list">
-        <Counter
-          title="Code Counter 💡"
-          count={count1}
-          onIncrement={handleIncrement}
-          onDecrement={handleDecrement}
-          onReset={handleReset}
-        />
-
-        <Counter
-          title="Passenger Counter"
-          count={count2}
-          onIncrement={handleIncrement1}
-          onDecrement={handleDecrement1}
-          onReset={handleReset1}
-        />
+        {counters.map((counter) => {
+          return (
+            <Counter
+              key={counter.id}
+              title={counter.title}
+              count={counter.count}
+              onIncrement={counter.onIncrement}
+              onDecrement={counter.onDecrement}
+              onReset={counter.onReset}
+            />
+          );
+        })}
       </div>
     </main>
   );
